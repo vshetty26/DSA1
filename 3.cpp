c20b7f8b54d85731b3c6aa1fca7c49f66ed1fc68
vamshishetty26 <vamshishetty26@gmail.com>
2025-03-19 21:27:43 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,29 @@
+// 3. Implement bubble sort to sort an array of integers.
+
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {50, 20, 40, 10, 30};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    bubbleSort(arr, size);
+    
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
