659f46b4b0d72e1c26b7fdf5a9ba2382ebf902d1
vamshishetty26 <vamshishetty26@gmail.com>
2025-03-21 22:47:35 +0530

    Add files via upload

---
+++ QB_practical/5.cpp
@@ -0,0 +1,31 @@
+//5. Write a program to sort an array using selection sort.
+
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main() {
+    int arr[] = {50, 20, 40, 10, 30};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    selectionSort(arr, size);
+    
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
